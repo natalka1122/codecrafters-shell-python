a7b50d406c1664de992823fed48127cd79f0794b
EXTERNAL Konovalova Natalia (EPAM, ETAS/ECI) <4388005+natalka1122@users.noreply.github.com>
2025-12-01 17:36:16 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,6 +11,13 @@ def main() -> None:
         if user_input_split[0] == "echo":
             sys.stdout.write(" ".join(user_input_split[1:]) + "\n")
             continue
+        if user_input_split[0] == "type":
+            for cmd in user_input_split[1:]:
+                if cmd in ["type", "echo", "exit"]:
+                    sys.stdout.write(f"{cmd} is a shell builtin\n")
+                else:
+                    sys.stdout.write(f"{cmd}: not found\n")
+            continue
         sys.stdout.write(f"{user_input}: command not found\n")
 
 
