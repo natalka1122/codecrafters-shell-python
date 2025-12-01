26f5dbe4629ca4aaa62729f7588065a217c04128
EXTERNAL Konovalova Natalia (EPAM, ETAS/ECI) <4388005+natalka1122@users.noreply.github.com>
2025-12-01 12:32:14 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ def main():
     while True:
         sys.stdout.write("$ ")
         user_input = input()
+        if user_input.strip() == "exit":
+            break
         sys.stdout.write(f"{user_input}: command not found\n")
 
 
