212c7c7b8152ecf6fbe940b9ab7fcc73e10ec2ce
EXTERNAL Konovalova Natalia (EPAM, ETAS/ECI) <4388005+natalka1122@users.noreply.github.com>
2025-12-01 12:31:19 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-
-    user_input = input()
-    sys.stdout.write(f"{user_input}: command not found")
+    while True:
+        sys.stdout.write("$ ")
+        user_input = input()
+        sys.stdout.write(f"{user_input}: command not found\n")
 
 
 if __name__ == "__main__":
