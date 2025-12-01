b1f413378e7051d380918f919ccd7413e177bc7d
EXTERNAL Konovalova Natalia (EPAM, ETAS/ECI) <4388005+natalka1122@users.noreply.github.com>
2025-12-01 17:43:23 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
+import os
 import sys
+from pathlib import Path
 
 
 def main() -> None:
@@ -15,7 +17,16 @@ def main() -> None:
             for cmd in user_input_split[1:]:
                 if cmd in ["type", "echo", "exit"]:
                     sys.stdout.write(f"{cmd} is a shell builtin\n")
-                else:
+                    continue
+                path = os.environ.get("PATH", "")
+                is_found = False
+                for path_dir in path.split(os.pathsep):
+                    file_path = Path(path_dir) / cmd
+                    if os.access(file_path, os.X_OK):
+                        is_found = True
+                        sys.stdout.write(f"{cmd} is {file_path}\n")
+                        break
+                if not is_found:
                     sys.stdout.write(f"{cmd}: not found\n")
             continue
         sys.stdout.write(f"{user_input}: command not found\n")
